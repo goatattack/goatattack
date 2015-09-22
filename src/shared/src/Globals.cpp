25513e0db0ece4f4ae4d9e8e7ca77f8b7288543a
freanux <pw@earthwave.ch>
2015-09-22 10:52:33 +0200

    set version to 0.4.2-dev

--- src/shared/src/Globals.cpp
+++ src/shared/src/Globals.cpp
@@ -1,6 +1,6 @@
 #include "Globals.hpp"
 
-const char *GameVersion = "0.4.1";
+const char *GameVersion = "0.4.2-dev";
 const char *UserDirectory = ".goatattack";
 const char *ConfigFilename = "config.conf";
 const bool ProductIsBeta = false;
