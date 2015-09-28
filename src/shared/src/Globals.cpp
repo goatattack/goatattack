ef9723cc750221c1e43fb31eabe67d35d1a9978b
Philippe Widmer <pw@earthwave.ch>
2015-09-28 21:51:27 +0200

    prepared to release 0.4.2

--- src/shared/src/Globals.cpp
+++ src/shared/src/Globals.cpp
@@ -1,6 +1,6 @@
 #include "Globals.hpp"
 
-const char *GameVersion = "0.4.2-dev";
+const char *GameVersion = "0.4.2";
 const char *UserDirectory = ".goatattack";
 const char *ConfigFilename = "config.conf";
 const bool ProductIsBeta = false;
