6eec3c22cdc52121f8bba1a3ae42a2af9aebcb69
freanux <pw@earthwave.ch>
2016-10-25 23:25:29 +0200

    corrected some window widths in due to overlaps of translated texts.

--- src/Engine/src/MainMenu.cpp
+++ src/Engine/src/MainMenu.cpp
@@ -284,7 +284,7 @@ void MainMenu::play_click() {
 
     int vw = subsystem.get_view_width();
     int vh = subsystem.get_view_height();
-    int ww = 400;
+    int ww = 460;
     int wh = 300;
 
     GuiWindow *window = push_window(vw / 2 - ww / 2, vh / 2 - wh / 2, ww, wh, i18n(I18N_MAINMENU_PLAY));
@@ -334,11 +334,11 @@ void MainMenu::play_click() {
     GuiFrame *frcustom = tab->create_tab(i18n(I18N_MAINMENU_CUSTOM_SERVER));
     create_label(frcustom, 0, 0, i18n(I18N_MAINMENU_ENTER_HOSTNAME));
     create_label(frcustom, 0, 21, i18n(I18N_MAINMENU_ADDRESS));
-    custom_ipaddress = create_textbox(frcustom, 70, 20, tab->get_width() - (2 * Spc) - 70 + 4, config.get_string("last_custom_address"));
+    custom_ipaddress = create_textbox(frcustom, 90, 20, tab->get_width() - (2 * Spc) - 90 + 4, config.get_string("last_custom_address"));
     create_label(frcustom, 0, 41, i18n(I18N_MAINMENU_PORT));
-    custom_port = create_textbox(frcustom, 70, 40, 100, config.get_string("last_custom_port"));
+    custom_port = create_textbox(frcustom, 90, 40, 100, config.get_string("last_custom_port"));
     create_label(frcustom, 0, 61, i18n(I18N_MAINMENU_PASSWORD));
-    custom_password = create_textbox(frcustom, 70, 60, tab->get_width() - (2 * Spc) - 70 + 4, "");
+    custom_password = create_textbox(frcustom, 90, 60, tab->get_width() - (2 * Spc) - 90 + 4, "");
     custom_password->set_hide_characters(true);
 
     create_button(frcustom, frlan->get_width() - bw_connect, frlan->get_height() - bh, bw_connect, bh, btn_connect, static_play_manual, this);
@@ -550,7 +550,7 @@ void MainMenu::create_server_click() {
     Font *f = get_font();
     int vw = subsystem.get_view_width();
     int vh = subsystem.get_view_height();
-    int ww = 337;
+    int ww = 357;
     int wh = 320;
     int bw = 140;
     GuiWindow *window = push_window(vw / 2 - ww / 2, vh / 2 - wh / 2, ww, wh, i18n(I18N_MAINMENU_LAN));
@@ -558,24 +558,24 @@ void MainMenu::create_server_click() {
 
     /* server settings */
     create_label(window, 15, 15, i18n(I18N_MAINMENU_SERVER_NAME2));
-    cs_server_name = create_textbox(window, 100, 15, 221, config.get_string("server_name"));
+    cs_server_name = create_textbox(window, 120, 15, 221, config.get_string("server_name"));
     cs_server_name->set_focus();
 
     create_label(window, 15, 35, i18n(I18N_MAINMENU_PORT));
-    cs_server_port = create_textbox(window, 100, 35, 55, config.get_string("server_port"));
+    cs_server_port = create_textbox(window, 120, 35, 55, config.get_string("server_port"));
 
     create_box(window, 15, 55, ww - 30, 1);
 
     /* game modes */
     int game_mode = config.get_int("game_mode");
     create_label(window, 15, 60, i18n(I18N_MAINMENU_GAME_MODE));
-    cs_dm = create_checkbox(window, 100, 61, i18n(I18N_MAINMENU_GM_DM), game_mode == GamePlayTypeDM, static_game_mode_click, this);
-    cs_tdm = create_checkbox(window, 100, 74, i18n(I18N_MAINMENU_GM_TDM), game_mode == GamePlayTypeTDM, static_game_mode_click, this);
-    cs_ctf = create_checkbox(window, 100, 87, i18n(I18N_MAINMENU_GM_CTF), game_mode == GamePlayTypeCTF, static_game_mode_click, this);
+    cs_dm = create_checkbox(window, 120, 61, i18n(I18N_MAINMENU_GM_DM), game_mode == GamePlayTypeDM, static_game_mode_click, this);
+    cs_tdm = create_checkbox(window, 120, 74, i18n(I18N_MAINMENU_GM_TDM), game_mode == GamePlayTypeTDM, static_game_mode_click, this);
+    cs_ctf = create_checkbox(window, 120, 87, i18n(I18N_MAINMENU_GM_CTF), game_mode == GamePlayTypeCTF, static_game_mode_click, this);
 
-    cs_sr = create_checkbox(window, 220, 61, i18n(I18N_MAINMENU_GM_SR), game_mode == GamePlayTypeSR, static_game_mode_click, this);
-    cs_ctc = create_checkbox(window, 220, 74, i18n(I18N_MAINMENU_GM_CTC), game_mode == GamePlayTypeCTC, static_game_mode_click, this);
-    cs_goh = create_checkbox(window, 220, 87, i18n(I18N_MAINMENU_GM_GOH), game_mode == GamePlayTypeGOH, static_game_mode_click, this);
+    cs_sr = create_checkbox(window, 240, 61, i18n(I18N_MAINMENU_GM_SR), game_mode == GamePlayTypeSR, static_game_mode_click, this);
+    cs_ctc = create_checkbox(window, 240, 74, i18n(I18N_MAINMENU_GM_CTC), game_mode == GamePlayTypeCTC, static_game_mode_click, this);
+    cs_goh = create_checkbox(window, 240, 87, i18n(I18N_MAINMENU_GM_GOH), game_mode == GamePlayTypeGOH, static_game_mode_click, this);
 
     cs_current_mode = cs_dm;
     if (game_mode == GamePlayTypeTDM) {
@@ -612,15 +612,15 @@ void MainMenu::create_server_click() {
 
     /* max players and duration */
     create_label(window, 15, 111, i18n(I18N_MAINMENU_MAX_PLAYERS));
-    cs_max_players = create_textbox(window, 100, 111, 55, config.get_string("max_players"));
+    cs_max_players = create_textbox(window, 120, 111, 55, config.get_string("max_players"));
 
     create_label(window, 15, 131, i18n(I18N_MAINMENU_WARMUP));
-    cs_warmup = create_textbox(window, 100, 131, 55, config.get_string("warmup"));
-    create_label(window, 160, 131, i18n(I18N_MAINMENU_IN_SECONDS));
+    cs_warmup = create_textbox(window, 120, 131, 55, config.get_string("warmup"));
+    create_label(window, 180, 131, i18n(I18N_MAINMENU_IN_SECONDS));
 
     create_label(window, 15, 151, i18n(I18N_MAINMENU_DURATION));
-    cs_duration = create_textbox(window, 100, 151, 55, config.get_string("duration"));
-    create_label(window, 160, 151, i18n(I18N_MAINMENU_IN_MINUTES));
+    cs_duration = create_textbox(window, 120, 151, 55, config.get_string("duration"));
+    create_label(window, 180, 151, i18n(I18N_MAINMENU_IN_MINUTES));
 
     create_box(window, 15, 171, ww - 30, 1);
 
@@ -639,7 +639,7 @@ void MainMenu::create_server_click() {
 
     create_label(window, 15, 177, i18n(I18N_MAINMENU_SELECT_MAP));
     int lbh = get_font()->get_font_height() * 6 + 2;
-    cs_maps = create_listbox(window, 15, 193, 235, lbh, "", static_map_selected, this);
+    cs_maps = create_listbox(window, 15, 193, 255, lbh, "", static_map_selected, this);
 
     fill_map_listbox(static_cast<GamePlayType>(game_mode));
 
